8e83d303e860cddf9c350648384cd9c6796f59a3
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2021-10-05 18:12:37 +0100

    Tidy Formatting of Day 1 2020

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,33 +1,34 @@
 targetValue =2020
 def load_data():
-    # load file values
-    with open('aoc-2020-1-input','r') as df:
-        lines = df.readlines()
+  # load file values
+  with open('aoc-2020-1-input','r') as df:
+      lines = df.readlines()
 
 # convert to int values
-    values =[]
-    for lineValue in lines:
-      values.append(int(lineValue))
-    return values
+  values =[]
+  for lineValue in lines:
+    values.append(int(lineValue))
+  return values
 
 
 values = load_data()
 
 def find_result(targetValue, values):
-    values.sort()
-    valueSet = set(values)
-    found = False
+  values.sort()
+  valueSet = set(values)
+  found = False
 
-    result = 0
+  result = 0
 
-    while not found and len(values) > 1:
-      startValue = values.pop()
-      searchValue = targetValue - startValue
-      if searchValue in valueSet:
-        found = True
-        # print('StartValue', startValue, 'SearchValue', searchValue)
-        result = searchValue * startValue
-    return result
+  while not found and len(values) > 1:
+    startValue = values.pop()
+    searchValue = targetValue - startValue
+    if searchValue in valueSet:
+      found = True
+      # print('StartValue', startValue, 'SearchValue', searchValue)
+      result = searchValue * startValue
+
+  return result
 
 
 result = find_result(targetValue, values)

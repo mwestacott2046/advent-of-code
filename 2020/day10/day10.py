1f93eea448bbdd5832e4f080840881ce923ae040
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2021-10-08 13:56:14 +0100

    2020 Day 9

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -19,10 +19,7 @@ def build_adapter_list(adapters):
   for adapter in sorted_list:
     diff = adapter - previous
     if diff > 3:
-      print('difference to big from {} to {} is {}'.format(previous, adapter, diff))
       return 0
-
-    print('start: {} adapter: {} diff: {}'.format(previous, adapter, diff))
     
     diffs[diff] = diffs.get(diff,0) +1
     previous = adapter
@@ -30,7 +27,6 @@ def build_adapter_list(adapters):
   #include jump to device
   diffs[device_interval] = diffs.get(device_interval,0) +1
 
-  print('diffs',diffs)
   return diffs.get(1,0) * diffs.get(3,0)
 
 

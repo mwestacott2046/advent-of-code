906eeea30941a73751cf1e910b6ae420024ce485
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2021-10-13 14:45:28 +0100

    2020 Day 13

--- 2020/day13/day13_pt2.py
+++ 2020/day13/day13_pt2.py
@@ -8,37 +8,34 @@ def load_timetable(filename):
   return depart_time, split_times
 
 
-def sequntial_bus_times (start_time, bus_offsets):
-  for bus_id, offset in bus_offsets.items():
-    if (start_time + offset) % bus_id != 0:
-      return False
+# def sequntial_bus_times (start_time, bus_offsets):
+#   for bus_id, offset in bus_offsets.items():
+#     if (start_time + offset) % bus_id != 0:
+#       return False
 
-  return True
+#   return True
 
 def find_departure_time (intervals):
-  bus_offsets = {}
+  bus_modulus = {}
 
   for time_offset, bus in enumerate(intervals):
     if bus != 'x':
       bus_int = int(bus)
-      bus_offsets[bus_int]= time_offset
+      bus_modulus[bus_int] = (bus_int - time_offset) % bus_int
 
-  print(bus_offsets)
+  # print(bus_modulus)
 
-  first_interval = int(intervals[0])
-  # print('first_interval', first_interval)
-  start_time = first_interval
+  vals = list(reversed(sorted(bus_modulus)))
+  val = bus_modulus[vals[0]]
+  print(vals)
+  r = vals[0]
+  for b in vals[1:]:
+      while val % b != bus_modulus[b]:
+          val += r
+      r *= b
+  return val
 
-  while not sequntial_bus_times (start_time, bus_offsets):
-    start_time += first_interval
-    # if start_time % 100000000 == 0:
-    #   print (start_time)
-
-  return start_time
-
-# This works for the smaller input, 
-# but takes forever with the large input, 
-# need to find a better way to do this!
+# Uses https://en.wikipedia.org/wiki/Chinese_remainder_theorem
 
 if __name__ == '__main__':
   # departure_time, intervals = load_timetable('test-input')
